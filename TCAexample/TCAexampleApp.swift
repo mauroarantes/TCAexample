5031114b738744d7399ab4d31940ba1cc4cc208d
Mauro Arantes <mauro.arantes@gmail.com>
2024-09-25 17:50:34 +0100

    Initial Commit

---
+++ TCAexample/TCAexampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCAexampleApp.swift
+//  TCAexample
+//
+//  Created by Mauro Arantes on 25/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct TCAexampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
